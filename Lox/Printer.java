d533581d5d3785e76507166476ff9e5e2b606ca6
carolCheng <chengle0575@foxmail.com>
2024-09-10 09:36:57 +0800

    fix astPrinter to print the tree structure

--- Lox/Printer.java
+++ Lox/Printer.java
@@ -4,40 +4,55 @@ import Lox.Exp.*;
 
 public class Printer implements Visitor {
 
+    int space=1;
+
+    Printer(){
+
+    }
+    Printer(int space){
+        this.space=space;
+    }
     @Override
     public String visit(Grouping grouping) {
+        space++;
+
         StringBuffer sb=new StringBuffer();
-        sb.append("group\n");
-        sb.append("/   ");
-        sb.append(grouping.exp.accept(new Printer()));
+        sb.append(" ".repeat(space)+"/group\n");
+        sb.append(" ".repeat(space)+grouping.exp.accept(new Printer(this.space)));
 
         return sb.toString();
     }
 
     @Override
     public String visit(Unary unary) {
+        space++;
+
         StringBuffer sb=new StringBuffer();
-        sb.append(unary.operator.type+"\n");
-        sb.append("/   ");
-        sb.append(unary.right.accept(new Printer()));
+        sb.append(" ".repeat(space)+"/"+unary.operator.type+"\n");
+        sb.append(" ".repeat(space)+unary.right.accept(new Printer(this.space)));
 
         return sb.toString();
     }
 
     @Override
     public String visit(Binary binary) {
+        space++;
+
         StringBuffer sb=new StringBuffer();
-        sb.append(binary.operator.type+"\n");
-        sb.append("/         \\");
-        sb.append(binary.left.accept(new Printer()));
-        sb.append("      ");
-        sb.append(binary.right.accept(new Printer()));
+        sb.append(" ".repeat(space)+"/"+binary.operator.type+"\n");
+        sb.append(" ".repeat(space)+binary.left.accept(new Printer(this.space)));
+
+        sb.append(" ".repeat(space)+binary.right.accept(new Printer(this.space)));
 
         return sb.toString();
     }
 
     @Override
     public String visit(Literal literal) {
-        return literal.value.toString()+"\n";
+        space++;
+        StringBuffer sb=new StringBuffer();
+        sb.append(" ".repeat(space)+"/literal --");
+        sb.append(" ".repeat(space)+literal.value.toString()+"\n");
+        return sb.toString();
     }
 }
