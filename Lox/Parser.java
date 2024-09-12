7ce9e7bd0aa33705057b3d2a26b03cc0d06d9eef
carolCheng <chengle0575@foxmail.com>
2024-09-12 17:40:05 +0800

    fix Parser bug. update to enable multiple operator like 1+1+1+

--- Lox/Parser.java
+++ Lox/Parser.java
@@ -37,10 +37,10 @@ public class Parser {
     }
 
     private Expression equality(){//equality-> comparison (((!=|==) comparison))*
-        System.out.println("current this.p is: "+this.p);
+
         Expression exp=comparison();
 
-        if(match(TokenType.BANG_EQUAL)||match(TokenType.EQUAL_EQUAL)){
+        while(match(TokenType.BANG_EQUAL)||match(TokenType.EQUAL_EQUAL)){
                 Token t=this.input.get(this.p);
                 moveahead();
                 Expression exp2= comparison();
@@ -51,10 +51,10 @@ public class Parser {
     }
 
     private  Expression comparison(){
-        System.out.println("current this.p is: "+this.p);
+
         Expression exp=term();
 
-        if(match(TokenType.GREATER)||match(TokenType.GREATER_EQUAL)||match(TokenType.LESS)||match(TokenType.LESS_EQUAL)){
+        while(match(TokenType.GREATER)||match(TokenType.GREATER_EQUAL)||match(TokenType.LESS)||match(TokenType.LESS_EQUAL)){
             Token t=this.input.get(this.p);
             moveahead();
             Expression exp2=term();
@@ -66,10 +66,10 @@ public class Parser {
 
 
     private Expression term(){
-        System.out.println("current this.p is: "+this.p);
+
         Expression exp=factor();
 
-        if(match(TokenType.PLUS)||match(TokenType.MINUS)){
+        while(match(TokenType.PLUS)||match(TokenType.MINUS)){
             Token t=this.input.get(this.p);
             moveahead();
             Expression exp2=factor();
@@ -80,10 +80,10 @@ public class Parser {
     }
 
     private Expression factor(){
-        System.out.println("current this.p is: "+this.p);
+
         Expression exp=unary();
 
-        if(match(TokenType.SLASH)||match(TokenType.STAR)){
+        while(match(TokenType.SLASH)||match(TokenType.STAR)){
             Token t=this.input.get(this.p);
             moveahead();
             Expression exp2=unary();
@@ -95,8 +95,8 @@ public class Parser {
     }
 
     private Expression unary(){
-        System.out.println("current this.p is: "+this.p);
-        if(match(TokenType.BANG)||match(TokenType.MINUS)){
+
+        while(match(TokenType.BANG)||match(TokenType.MINUS)){
             Token t=this.input.get(this.p);
             moveahead();
             return new Unary(t,unary());
@@ -106,7 +106,6 @@ public class Parser {
     }
 
     private Expression primary(){
-        System.out.println("current this.p is: "+this.p);
 
         if(match(TokenType.NUMBER)||match(TokenType.STRING)){
             moveahead();
@@ -130,8 +129,7 @@ public class Parser {
             //find the next right brace
            int pt=this.input.size()-1;
            while(pt>this.p){
-               if(this.input.get(pt).type==TokenType.RIGHT_BRACE){
-                   //find the outer brace
+               if(this.input.get(pt).type==TokenType.RIGHT_BRACE){//find the outer brace
                    break;
                }
                pt--;
@@ -140,7 +138,7 @@ public class Parser {
            moveahead();
 
            if(pt>this.p && pt==this.input.size()-1){
-               return new Grouping(expression());
+               return new Grouping(new Parser(this.input.subList(p,pt)).generateAST());
            } else if(pt>this.p){
                return new Binary(new Grouping(expression()),this.input.get(pt+1),new Parser(this.input.subList(pt+2,this.input.size())).generateAST());
            }
