658ef3eb222479473b4c58d0a200bf3a49458597
carolCheng <chengle0575@foxmail.com>
2024-10-09 17:52:36 +0800

    chain the enviroment to enable nested env

--- Lox/Environment.java
+++ Lox/Environment.java
@@ -6,9 +6,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Environment {
+    private Environment outerEnv; //use a pointer to chain the environments
     private Map<String, Object> map;
 
     public Environment(){
+        map=new HashMap<>();
+    }
+
+    public Environment(Environment e){
+        this.outerEnv=e;
         map=new HashMap<>();
     }
 
@@ -17,8 +23,13 @@ public class Environment {
     }
 
     public Object get(Token t){
+
         if(map.containsKey(t.literal)) return map.get(t.literal);
-        throw new RuntimeError(t,"The variable is undefined");
+        //should recursively find the key, towards outer environment
+        if(outerEnv==null)
+            throw new RuntimeError(t,"The variable is undefined");
+        else
+            return outerEnv.get(t);
     }
 
 }
